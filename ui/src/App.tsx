bbb266975f5c9561d55595d1c736333a01cb0be3
T J <thierry@janaudy.com>
2026-02-20 09:50:17 +0100

    Fixed image paths

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,7 +6,7 @@ import Layout from '@/components/Layout'
 
 function App() {
     return (
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
             <Layout>
                 <Routes>
                     <Route path="/" element={<WelcomePage />} />
